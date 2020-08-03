fc139d7ce46660d73cf3670e1fb0f510f12b0ee6
Nir <nir61202@gmail.com>
2020-08-03 17:20:04 +0300

    Add ncandasmsetting and serializable setting

--- command_serializer.py
+++ command_serializer.py
@@ -1,4 +1,5 @@
-from .constants import *
+from constants import *
+
 
 def escape_specials(data: bytes) -> bytes:
     arr = bytearray()
@@ -7,8 +8,10 @@ def escape_specials(data: bytes) -> bytes:
         arr.extend(SPECIAL_CHARS.get(byte, (byte)))
     return bytes(arr)
 
+
 def sum_checksum(data: bytes) -> bytes:
     return int.to_bytes(sum(data) & 255, 1, "little")
+
 
 def package_data_for_bt(data_type: int, command_data: bytes, unk: int) -> bytes:
     """
@@ -18,12 +21,10 @@ def package_data_for_bt(data_type: int, command_data: bytes, unk: int) -> bytes:
     * CommandBluetoothSender:sendCommandWithRetries
     * BluetoothSenderWrapper.sendCommandViaBluetooth
     # 
-    """ 
-    if data_type > 255 or unk > 255 or data_type < 0 or unk < 0:
-        raise Exception("dataType and unk must be between 0 and 255")
+    """
+    data_type_byte = ntb(data_type)
+    unk_byte = ntb(unk)
 
-    data_type_byte = int.to_bytes(data_type, 1, "little")
-    unk_byte = int.to_bytes(unk, 1, "little")
     data_size_big_endian = int.to_bytes(len(command_data), 4, "big")
     data_to_check = data_type_byte + unk_byte + data_size_big_endian + command_data
     checksum = sum_checksum(data_to_check)
@@ -32,7 +33,50 @@ def package_data_for_bt(data_type: int, command_data: bytes, unk: int) -> bytes:
 
     # Message will be chunked if it's larger than MAX_BLUETOOTH_MESSAGE_SIZE, just crash to deal with it for now
     if len(final_command) > MAX_BLUETOOTH_MESSAGE_SIZE:
-        raise Exception(f"Exceeded {MAX_BLUETOOTH_MESSAGE_SIZE}, can't handle chunked messages")
+        raise Exception(
+            f"Exceeded {MAX_BLUETOOTH_MESSAGE_SIZE}, can't handle chunked messages"
+        )
 
     return final_command
-    
+
+
+class NcAndAsmSetting:
+    def __init__(
+        self,
+        nc_asm_effect: NC_ASM_EFFECT,
+        nc_asm_setting_type: NC_ASM_SETTING_TYPE,
+        unk_nc_value,
+        asm_setting_type: ASM_SETTING_TYPE,
+        asm_id: ASM_ID,
+        asm_level: int,
+    ):
+        self.nc_asm_effect = nc_asm_effect
+        self.nc_asm_setting_type = nc_asm_setting_type
+        self.unk_nc_value = unk_nc_value
+        self.asm_setting_type = asm_setting_type
+        self.asm_id = asm_id
+        self.asm_level = ntb(asm_level)
+
+    def to_bytes(self) -> bytes:
+        return (
+            NC_ASM_INQUIRED_TYPE.NOISE_CANCELLING_AND_AMBIENT_SOUND_MODE.value
+            + self.nc_asm_effect.value
+            + self.nc_asm_setting_type.value
+            + self.unk_nc_value
+            + self.asm_setting_type.value
+            + self.asm_id.value
+            + self.asm_level
+        )
+
+class SerializableNcAsmSetting:
+    def __init__(
+        self,
+        setting: NcAndAsmSetting
+    ):
+        self.setting = setting
+
+    def to_bytes(self) -> bytes:
+        return Command.NCASM_SET_PARAM.value + self.setting.to_bytes()
+
+
+# class SerializableNcAsmSetting()
