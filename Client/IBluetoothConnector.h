0a1102ed177058f88e10a1175b0de6bb7c43a349
Nir <nir61202@gmail.com>
2020-08-08 01:42:24 +0300

    Make noncopyable

--- Client/IBluetoothConnector.h
+++ Client/IBluetoothConnector.h
@@ -6,7 +6,14 @@ inline constexpr auto XM3_UUID = "96CC203E-5068-46ad-B32D-E316F5E069BA";
 class IBluetoothConnector
 {
 public:
+	IBluetoothConnector() = default;
 	virtual ~IBluetoothConnector() = default;
+
+	IBluetoothConnector(const IBluetoothConnector&) = delete;
+	IBluetoothConnector(IBluetoothConnector&&) = delete;
+
+	IBluetoothConnector& operator=(const IBluetoothConnector&) = delete;
+	IBluetoothConnector& operator=(IBluetoothConnector&&) = delete;
 
 	virtual int send(char* buf, size_t length) = 0;
 };
