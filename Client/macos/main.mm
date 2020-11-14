ced304b2d71054554820e069d9cd593802499d48
semvis123 <semvisscher10@gmail.com>
2020-11-14 21:57:35 +0100

    Cleaned headers

--- Client/macos/main.mm
+++ Client/macos/main.mm
@@ -1,18 +1,5 @@
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_metal.h"
 #include <stdio.h>
 
-#define GLFW_INCLUDE_NONE
-#define GLFW_EXPOSE_NATIVE_COCOA
-#include <GLFW/glfw3.h>
-#include <GLFW/glfw3native.h>
-
-#import <Metal/Metal.h>
-#import <QuartzCore/QuartzCore.h>
-
-#include "Constants.h"
-#include "TimedMessageQueue.h"
 #include "MacOSGUI.h"
 #include "MacOSBluetoothConnector.h"
 #include "BluetoothWrapper.h"
