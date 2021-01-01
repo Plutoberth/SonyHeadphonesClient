a4f077ad089031bc78ebf61a1c1ad7d55a5ae8e3
semvis123 <semvisscher10@gmail.com>
2021-01-02 00:29:44 +0100

    Working, except disconnect button

--- Client/macos/ViewController.mm
+++ Client/macos/ViewController.mm
@@ -8,7 +8,7 @@
 #import "ViewController.h"
 
 @implementation ViewController
-@synthesize connectedLabel, connectButton, ANCSlider, ANCValueLabel;
+@synthesize connectedLabel, connectButton, ANCSlider, ANCValueLabel, focusOnVoice, focusOnVoiceLabel;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -20,7 +20,7 @@
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
-
+    
     // Update the view, if already loaded.
 }
 
@@ -28,6 +28,7 @@
     if (bt.isConnected()){
         bt.disconnect();
         [ANCSlider setEnabled:FALSE];
+        [focusOnVoice setEnabled:FALSE];
         [connectButton setTitle:@"Connect to Bluetooth device"];
         [connectedLabel setStringValue:@"Not connected"];
     }
@@ -48,12 +49,41 @@
             [connectedLabel setStringValue:[@"Connected: " stringByAppendingString:[device nameOrAddress]]];
             [connectButton setTitle:@"Disconnect"];
             [ANCSlider setEnabled:TRUE];
+            [focusOnVoice setEnabled:FALSE];
         }
     }
 }
 - (IBAction)sliderChanged:(id)sender {
-    NSLog(@"%@", ANCSlider.stringValue);
     [ANCValueLabel setStringValue:ANCSlider.stringValue];
-}
+    if (ANCSlider.intValue >= MINIMUM_VOICE_FOCUS_STEP)
+    {
+        [focusOnVoiceLabel setStringValue:@"Focus on Voice"];
+        [focusOnVoice setEnabled:TRUE];
+    }
+    else
+    {
+        [focusOnVoiceLabel setStringValue:@"Focus on Voice isn't enabled on this level."];
+        [focusOnVoice setEnabled:FALSE];
+    }
+    auto ncAsmEffect = NC_ASM_EFFECT::ADJUSTMENT_COMPLETION;
+    auto asmId = focusOnVoice.state == NSControlStateValueOn ? ASM_ID::VOICE : ASM_ID::NORMAL;
+    bt.sendCommand(CommandSerializer::serializeNcAndAsmSetting(
+                                                               ncAsmEffect,
+                                                               NC_ASM_SETTING_TYPE::LEVEL_ADJUSTMENT,
+                                                               ASM_SETTING_TYPE::LEVEL_ADJUSTMENT,
+                                                               asmId,
+                                                               ANCSlider.intValue
+                                                               ));}
+- (IBAction)voiceChanged:(id)sender {
+    [ANCValueLabel setStringValue:ANCSlider.stringValue];
+    auto ncAsmEffect = NC_ASM_EFFECT::ADJUSTMENT_COMPLETION;
+    auto asmId = focusOnVoice.state == NSControlStateValueOn ? ASM_ID::VOICE : ASM_ID::NORMAL;
+    bt.sendCommand(CommandSerializer::serializeNcAndAsmSetting(
+                                                               ncAsmEffect,
+                                                               NC_ASM_SETTING_TYPE::LEVEL_ADJUSTMENT,
+                                                               ASM_SETTING_TYPE::LEVEL_ADJUSTMENT,
+                                                               asmId,
+                                                               ANCSlider.intValue
+                                                               ));}
 
 @end
